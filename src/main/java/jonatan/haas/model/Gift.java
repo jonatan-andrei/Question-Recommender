6c71fecb3a0c641ddbab3349e38a6938ad9ba51e
Jonatan Andrei Haas <jonatan.haas@cwi.com.br>
2022-07-18 07:59:26 -0300

    Adicionado Lombok

--- src/main/java/jonatan/haas/model/Gift.java
+++ src/main/java/jonatan/haas/model/Gift.java
@@ -1,31 +1,21 @@
 package jonatan.haas.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 
 @Entity
+@Getter
+@Setter
 public class Gift {
-    private Long id;
-    private String name;
 
     @Id
     @SequenceGenerator(name = "giftSeq", sequenceName = "gift_id_seq", allocationSize = 1, initialValue = 1)
     @GeneratedValue(generator = "giftSeq")
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    private Long id;
+    private String name;
 }
